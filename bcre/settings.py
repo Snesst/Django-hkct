9a190df74cd001af56f4df25a75619e63d824f76
snesst <hungyufai@gmail.com>
2024-09-04 12:23:38 +0800

    modals generated

--- bcre/settings.py
+++ bcre/settings.py
@@ -81,8 +81,11 @@ WSGI_APPLICATION = 'bcre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bcre',
+        'USER': 'postgres',
+        'PASSWORD': '403791',
+        'HOST': 'localhost',
     }
 }
 
--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from datetime import datetime
+from realtors.models import Realtor
 
 # Create your models here.
+class Listing(models.Model):
+    realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    street = models.CharField(max_length=200)
+    district = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+    bedrooms = models.IntegerField()
+    bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
+    clubhouse = models.IntegerField()
+    sqft = models.IntegerField()
+    estate_size = models.FloatField(default=0989)
+    is_published = models.BooleanField(default=True)
+    list_date = models.DateTimeField(auto_now_add=True) #auto date from the system
+    photo_main = models.ImageField (upload_to='photos/%Y/%m/%d/') #essential photo
+    photo_1 = models.ImageField (upload_to='photos/%Y/%m/%d/', blank=True) #optional photos with blank = True
+    photo_2 = models.ImageField (upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_3 = models.ImageField (upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_4 = models.ImageField (upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_5 = models.ImageField (upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_6 = models.ImageField (upload_to='photos/%Y/%m/%d/', blank=True)
+
+class Realtor(models.Model):
+    name = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d?')
+    description = models.TextField(blank=True)
+    phone = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+    is_mvp = models.BooleanField(default=False)
+    hire_date = models.DateTimeField(default=datetime.now, blank=True)
+
+#return "title" of the housing in class Listing
+    def __str__(self):
+        return self.title #__str__ is an internal variable to show value of class listing, here show the "title" instead.
