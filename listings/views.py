8759bb1230702f9ecd1ed9b4d7e2b229606a47c0
snesst <hungyufai@gmail.com>
2024-09-10 22:24:07 +0800

    10 Sept 2024: searching half completed

--- listings/views.py
+++ listings/views.py
@@ -1,20 +1,69 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from .models import Listing
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from listings.choices import price_choices, bedroom_choices, district_choices
 # Create your views here.
 
 def index(request):
     # get all data from listing database
-    listings = Listing.objects.all()
+    #listing_id = Listing.objects.all()
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings, 3)
+
+    #GET is html method while get is a function.
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+    
     # pass database records into listings context. Make a dictionary
-    context = {'listings': listings}
+    context = {'listings': paged_listings}
     return render(request, 'listings/listings.html', context) 
 # the first listings is the app name. The second listings is the url name, define a dictionary with key = 'name', and value = 'something'.
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context ={'listing': listing}
+    return render(request, 'listings/listing.html', context)
 # Create your views here.
 
 def search(request):
-    return render(request, 'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date')
+    if 'keywords' in request.GET: # check if existence of key words in search
+        keywords = request.GET['keywords'] #if words entered, put such words into "keywords"
+        if keywords:
+            queryset_list = queryset_list.filter(
+                description__icontains=keywords #icontains means case insensitive
+            )
+    if 'title' in request.GET: # check if existence of key words in search
+        title = request.GET['title'] #if words entered, put such words into "keywords"
+        if title:
+            queryset_list = queryset_list.filter(
+                title__icontains=title 
+            )
+    if 'District' in request.GET: # check if existence of key words in search
+        District = request.GET['District'] #if words entered, put such words into "keywords"
+        if District:
+            queryset_list = queryset_list.filter(
+                district__iexact=District #means exact
+            )
+    if 'price' in request.GET: # check if existence of key words in search
+        price = request.GET['price'] #if words entered, put such words into "keywords"
+        if price:
+            queryset_list = queryset_list.filter(
+                price__lte=price #lte mean less than and equal
+            )
+    if 'bedrooms' in request.GET: # check if existence of key words in search
+        bedrooms = request.GET['bedrooms'] #if words entered, put such words into "keywords"
+        if bedrooms:
+            queryset_list = queryset_list.filter(
+                bedrooms__lte=bedrooms #lte mean less than equal
+            )
+
+    context = {
+        'district_choices': district_choices,
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices,
+        'listings': queryset_list
+
+    }
+    return render(request, 'listings/search.html', context)
 # Create your views here.
